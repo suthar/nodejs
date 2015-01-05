86d126ca10ac53f31236bebd2d473d6cc00ab3d8
Amit Suthar <the.aks.amit@gmail.com>
2015-01-05 18:32:04 +0530

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var server = require('./server');
 var router = require('./router');
 var requestHandler = require('./requestHandler')
 var handle = {}
+
 handle["/"] = requestHandler.start
 handle["/start"] = requestHandler.start
 handle["/upload"] = requestHandler.upload
